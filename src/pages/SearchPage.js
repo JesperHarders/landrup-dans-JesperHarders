bc4bcdaa3ea01c5a29f8316fcc81cb49dc1a2778
JesperHarders <4160144@RTS-365.dk>
2022-10-07 13:44:16 +0200

    Remove code that were commented out

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -32,15 +32,6 @@ const SearchPage = () => {
         }
     })
 
-    // const noSearch = () => {
-    //     if(isLoaded === true && searchData === ''){
-    //         return <p>Der blev ikke fundet nogle aktiviteter. Prøv at søge efter noget andet.</p>
-    //     } else {
-    //         return <ActivityCards/>
-    //     }
-    // }
-    
-    // const test = noSearch
     return (
         <div className="bg-purple p-[30px] h-[100%]">
             <PageHeading text='Søg'/>
@@ -48,20 +39,7 @@ const SearchPage = () => {
                 <input onChange={handleSearch} type='text' className="w-[100%] h-[40px] bg-[#C4C4C4] opacity-50"/>
                 <FiSearch className="text-24 text-white absolute right-[10px] bottom-[3px]"/>
             </form>
-            {/* {searchData ? filteredData.map((data) => (
-                <Link to={`/activity/${data.id}`} key={data.id}>
-                <div className='relative w-[100%] h-[50vh] mb-[30px]'>
-                    <img src={data.asset.url} className='w-[100%] h-[100%] object-cover rounded-bl-[50px] rounded-tr-[50px] rounded-tl-[50px]' alt='Activity'/>
-                    <div className="w-[100%] h-[25%] bg-pink bottom-[0px] absolute rounded-bl-[50px] rounded-tr-[50px] opacity-90 py-[10px] px-[20px]">
-                        <p className="text-18">{data.name}</p>
-                        <p className="text-18">{data.minAge}-{data.maxAge} år</p>
-                    </div>
-                </div>
-            </Link>
-            )) : isLoaded ? <p>Der blev ikke fundet nogle aktiviteter. Prøv at søge efter noget andet.</p> : <ActivityCards/>} */}
             {!searchData && <ActivityCards/>}
-            {/* {isLoaded && !searchData && <p>Der blev ikke fundet nogle aktiviteter. Prøv at søge efter noget andet.</p>} */}
-            {/* {noResults && <p>Der blev ikke fundet nogle aktiviteter. Prøv at søge efter noget andet.</p>} */}
             {searchData && filteredData.map((data) => (
                 <Link to={`/activity/${data.id}`} key={data.id}>
                 <div className='relative w-[100%] h-[50vh] mb-[30px]'>
